c031debfd97b89f3db1bf6a286fb01b0b1567c8e
s0044613 <sama.mushtaq@rgare.com>
2018-10-13 01:33:24 -0500

    more css chnages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+const ABOUT_ME_TEXT =`
+<h2> About Me:</h2>
+<p>
+Sama is a word of Arabic origin meaning "sky".<p><p>
+I am student of Udacity and a Software developer by profession.<p>
+When I am not working or coding, I play with my cats named Purrbles,Boo and Nacho.<p><p>
+In my spare time I also like to cook and bake. Home-made cookies are my speciality.<p><p>
+I am  also an amateur aquarium-keeper and have a fish named Dragontail.<p><p>
+I love to travel and explore new places as well
+<p><p>
+My favorite quote:
+
+<p>Eager eye and willing ear,
+Lovingly shall nestle near.
+<p><p>
+In a Wonderland they lie,
+<p>Dreaming as the days go by,
+<p>Dreaming as the summers die:
+<p><p>
+<p>Ever drifting down the stream —
+<p>Lingering in the golden gleam —
+<p>Life, what is it but a dream?
+<p>-- Lewis Caroll`;
+
 let profilePicturePath = `images/sm.jpg`;
 let logoPath = `images/logo.png`;
 
@@ -18,27 +42,28 @@ let hobbiesTextArray = [
   `Home-made cookies,compliments to the chef`,
   `An aerial view of Denver city, Colorado.`
 ];
-let hobbyCaptions= [`Aquarium Keeping`, `Baking` ,`Traveling`];
+let hobbyCaptions = [`Aquarium Keeping`, `Baking`, `Traveling`];
 
 catImageArray.forEach(cat => {
 
+  const catName = cat.replace(/^.*[\\\/]/, '').replace(".jpg","").trim();
   const catImage = document.createElement('img');
   catImage.setAttribute('src', cat);
-  catImage.setAttribute('alt', `Picture of my cat ${cat.replace(/^.*[\\\/]/, '')}`);
-  catImage.classList.add("cat-image-class");
+  catImage.setAttribute('alt', `Picture of my cat ${catName}`);
+  catImage.classList.add("zoom");
 
-  const catImageDiv = document.createElement('div');
-  catImageDiv.classList.add("catImage-div-class");
-  catImageDiv.append(catImage);
+  const catImageFigure = document.createElement('figure');
+  catImageFigure.setAttribute('aria-role', "presentation");
+  catImageFigure.append(catImage);
 
-  const textContainer = document.createElement('div');
-  textContainer.classList.add("text-container-class");
-  textContainer.innerHTML = cat.replace(/^.*[\\\/]/, '');
-  catImageDiv.append(textContainer);
+  const catImageCaption = document.createElement('figcaption');
+  catImageCaption.innerHTML = catName;
+
+  catImageFigure.append(catImageCaption);
 
   let divider = document.getElementsByClassName("cat-class-div")[0];
 
-  divider.append(catImageDiv);
+  divider.append(catImageFigure);
 
 });
 
@@ -62,12 +87,12 @@ pageHeader.append(pageSubHeading);
 
 //create main article
 const aboutMe = document.createElement('article');
-aboutMe.innerHTML = `
-Sama is a word of Arabic origin meaning "sky".<p>
-I live in the state of Missouri. I have 3 cats named Purrbles,Boo and Nacho. I am an amateur aquarium-keeper.
-I also have a fish named Dragontail.<p>
-I am student of Udacity and a Software developer by profession. `;
+aboutMe.setAttribute('aria-role',"contentinfo");
+aboutMe.setAttribute('aria-label',"A short bio");
+
+aboutMe.innerHTML =ABOUT_ME_TEXT;
 const mainContent = document.getElementsByClassName("main-class-div")[0];
+mainContent.setAttribute('aria-role',"main");
 mainContent.append(aboutMe);
 
 const aboutMeImg = document.createElement('img');
@@ -85,16 +110,23 @@ hobbyContent.append(hobbiesText);
 
 let index = 0;
 hobbiesArray.forEach(hobby => {
-    const hobbyImage = document.createElement('img');
-    hobbyImage.setAttribute('src', hobby);
-    hobbyImage.setAttribute('alt', hobbiesTextArray[index]);
+  const hobbyImage = document.createElement('img');
+  hobbyImage.setAttribute('src', hobby);
+  hobbyImage.setAttribute('alt', hobbiesTextArray[index]);
+  hobbyImage.classList.add('zoom');
 
-const hobbyTitle = document.createElement('figure');
-hobbyTitle.setAttribute('aria-role',"presentation");
-const caption = document.createElement('figcaption');
-caption.innerHTML = hobbyCaptions[index++];
-hobbyTitle.append(hobbyImage);
-hobbyTitle.append(caption);
+  const hobbyTitle = document.createElement('figure');
+  hobbyTitle.setAttribute('aria-role', "presentation");
+  const caption = document.createElement('figcaption');
+  caption.innerHTML = hobbyCaptions[index++];
+  hobbyTitle.append(hobbyImage);
+  hobbyTitle.append(caption);
 
-hobbyContent.append(hobbyTitle);
+  hobbyContent.append(hobbyTitle);
 });
+
+
+//add content to footer
+
+const footer = document.getElementById("footer");
+footer.innerHTML =`Let's connect and keep in <a href="https://www.linkedin.com/in/sama-mushtaq-baa265127/"> touch`;
